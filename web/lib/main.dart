dcaa36d242649df0a7add2a0a47feeb8ba11fdd4
wablerfam <wablerfam@gmail.com>
2025-12-30 08:56:37 +0900

    wip

--- web/lib/main.dart
+++ web/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CounterNotifier extends Notifier<int> {
+  @override
+  int build() => 0;
+
+  void increment() => state++;
+}
+
+final counterProvider = NotifierProvider<CounterNotifier, int>(
+  CounterNotifier.new,
+);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,47 +31,70 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends ConsumerStatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyHomePageState extends ConsumerState<MyHomePage> {
+  Offset _buttonPosition = const Offset(100, 100);
 
   @override
   Widget build(BuildContext context) {
+    final counter = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('You have pushed the button this many times:'),
+                Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          Positioned(
+            left: _buttonPosition.dx,
+            top: _buttonPosition.dy,
+            child: Draggable<String>(
+              data: 'button',
+              feedback: FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+              childWhenDragging: FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: Colors.grey,
+                child: const Icon(Icons.add),
+              ),
+              onDragEnd: (details) {
+                setState(() {
+                  final renderBox = context.findRenderObject() as RenderBox;
+                  final localPosition = renderBox.globalToLocal(details.offset);
+                  _buttonPosition = localPosition;
+                });
+              },
+              child: FloatingActionButton(
+                onPressed: () => ref.read(counterProvider.notifier).increment(),
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
